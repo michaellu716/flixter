1374a859a15c9ae33e478268e838cdbdaece0c72
Michael Lu <michael_108@hotmail.com>
2019-07-08 12:26:20 +0000

    fixed permission issues

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,6 @@
 class LessonsController < ApplicationController
 	before_action :authenticate_user!
-	before_action :require_authorized_for_current_lesson
+	before_action :require_authorized_for_current_lesson, only: [:show]
 
 	def show
 		current_lesson
@@ -15,9 +15,10 @@ class LessonsController < ApplicationController
 	end
 
 	def require_authorized_for_current_lesson
-     if current_lesson.section.course.user != current_user
+    unless current_user.enrolled_in?(current_lesson.section.course)
       redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled to view that lesson.'
     end
   end
 
 end
+
